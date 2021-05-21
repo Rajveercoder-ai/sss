8883b8c635906d46eb550cab07e1916a62ad3f88
Rajveercoder-ai <79631594+Rajveercoder-ai@users.noreply.github.com>
2021-05-21 18:14:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var  hyball,database,position;
+
+function setup(){
+    database = firebase.database();
+    console.log(database)
+    createCanvas(500,500);
+    hyball = createSprite(250,250,10,10);
+    hyball.shapeColor = "red";
+    hyballPosition=database.ref('ball/position')
+    hyballPosition.on("value",readPosition)
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined)
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+    writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPosition(data){
+    position=data.val();
+    hyball.x=position.x;
+    hyball.y=position.y
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x ,
+        'y': position.y + y
+    })
+}
